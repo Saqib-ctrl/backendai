e523b835615b12b19fd20d25a839f9d5abac4cfb
Muhmmad Najam Us Saqib <bc210411582mnu@vu.edu.pk>
2025-05-03 10:28:14 +0500

    Update application_router.py

--- routers/application_router.py
+++ routers/application_router.py
@@ -1,9 +1,9 @@
 
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
-from ..database import SessionLocal
-from backend import schemas
-from ..services import application_service
+from database import SessionLocal
+import schemas
+from services import application_service
 
 router = APIRouter()
 
